d5e6737fc0aa71154e4161225f8d3ed877c58d82
LAK132 <lakridesagain@gmail.com>
2026-05-27 20:16:48 +0930

    update yshtola to 26.05

--- systems/yshtola/flake.nix
+++ systems/yshtola/flake.nix
@@ -1,6 +1,6 @@
 {
 	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 		components.url = ./../../components;
 	};
 
@@ -8,6 +8,14 @@
 		config = args: nixpkgs.lib.nixosSystem (args // {
 			system = "x86_64-linux";
 			modules = args.modules ++ [
+				{nixpkgs.overlays = [
+					(_: prev: {
+						openldap = prev.openldap.overrideAttrs {
+							# https://github.com/NixOS/nixpkgs/issues/513245
+							doCheck = !prev.stdenv.hostPlatform.isi686;
+						};
+					})
+				];}
 				./configuration.nix
 				components.lak132
 				components.display.plasma
