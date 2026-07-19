9d10f7b8afb6c9c0864d1451154917c92219adfc
LAK132 <lakridesagain@gmail.com>
2026-07-19 22:38:21 +0930

    add halmarut system

---
+++ systems/halmarut/configuration.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, ... }:
+
+{
+	# --- boot ---
+
+	boot = {
+		supportedFilesystems = [ "ntfs" ];
+	};
+
+	# --- networking ---
+
+	networking = {
+		hostName = "halmarut";
+		# hostId = (builtins.substring 0 8 (builtins.readFile "/etc/machine-id"));
+		networkmanager.enable = true;
+	};
+
+	# --- time ---
+
+	time.timeZone = "Australia/Adelaide";
+
+	# --- hardware ---
+
+	# --- software ---
+
+	nixpkgs.config.allowUnfree = true;
+
+	# enable CUPS to print documents
+	services.printing.enable = true;
+}
---
+++ systems/halmarut/flake.nix
@@ -0,0 +1,20 @@
+{
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+		components.url = ./../../components;
+	};
+
+	outputs = { self, nixpkgs, components }: {
+		config = args: nixpkgs.lib.nixosSystem (args // {
+			system = "x86_64-linux";
+			modules = args.modules ++ [
+				./configuration.nix
+				components.lak132
+				components.display.mate
+				components.hardware.bluetooth
+				components.software.dev
+				components.software.internet
+			];
+		});
+	};
+}
